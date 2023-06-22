b2e4cfba7feffdb7a386ced830fc3ae8a4ac8fd3
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2023-06-22 09:55:11 -0300

    Feat: Criando autenticacao de usuario

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,3 +1,24 @@
-class AuthenticateUserService {
+// responsavel por autenticar o usuario
+import { UsersRepositories } from "../repositories/UsersRepositories";
+import { compare } from "bcryptjs";
 
+interface IAuthenticateRequest {
+    email: string;
+    password: string;
 }
+class AuthenticateUserService {
+    async execute({ email, password }: IAuthenticateRequest ) {
+        const user = await  UsersRepositories.findOneBy({ email });
+
+        // verificar se email existe
+        if (!user) {
+            throw new Error('Email/password incorrect.');
+        }
+
+        // verificar se senha esta correta
+
+        // gerar token
+    }
+}
+
+export { AuthenticateUserService };
